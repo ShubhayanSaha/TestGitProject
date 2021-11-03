305b293dc3f9e93c2ea9c62b791f8846f24f38e2
shubh <shubh@DESKTOP-5OR6675>
2021-11-03 15:28:12 +0530

    Commit 1

--- TestGit1/src/TestGit.java
+++ TestGit1/src/TestGit.java
@@ -3,7 +3,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("!!!******Hello India!!!");
-		System.out.println("Have to do rigorous testing");
+		System.out.println("Have to do rigorous testing123");
 	}
 
 }
